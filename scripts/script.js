0333f71f5429efc4bb6bed7912ed31bd417148ac
Lucas Viana <lcasviana@gmail.com>
2020-06-03 22:35:36 -0300

    Fixing verify end function

--- scripts/script.js
+++ scripts/script.js
@@ -46,7 +46,7 @@
 
   function verifyEndGame() {
     checkLine(board[0][0], board[1][1], board[2][2]);
-    checkLine(board[2][2], board[1][1], board[0][0]);
+    checkLine(board[0][2], board[1][1], board[2][0]);
     for (let i = 0; i < 3; i++) {
       checkLine(board[i][0], board[i][1], board[i][2]);
       checkLine(board[0][i], board[1][i], board[2][i]);
